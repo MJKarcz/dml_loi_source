f41122655664faa89538c28513e5beef979243bc
KARCZ Maciej 268296 <maciej.karcz@cea.fr>
2022-10-12 12:57:36 +0200

    commit

--- melange/item.py
+++ melange/item.py
@@ -76,7 +76,7 @@ class Item:
     def calculate_price(self) -> float:
         # remember that instance can have its own copy of a global attribute, that is why you don't call Item.pay_rate
         new_price = self.__price * self.pay_rate
-        print(f'This is a new price: {new_price}')
+        print(f'This is a new price22222: {new_price}')
         return new_price
 
 if __name__ == "__main__":
