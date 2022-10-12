a3b381943c1f1e54f4ec48208986e4abf55d2be6
KARCZ Maciej 268296 <maciej.karcz@cea.fr>
2022-10-12 11:48:51 +0200

    delete main

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dml_loi_source',
+    version='1',
+    packages=['melange'],
+    url='',
+    license='',
+    author='MK268296',
+    author_email='',
+    description='Set of tools for the dml_loi project'
+)
